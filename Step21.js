c604cbfc87acb1da12b6fba2fcc10862f4eec953
withdxng <breadnguyen2504@gmail.com>
2024-06-14 11:13:27 +0700

    Create Step21.js
    https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/learn-introductory-javascript-by-building-a-pyramid-generator/step-21

---
+++ Step21.js
@@ -0,0 +1,8 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+rows[2] = 10;
+console.log(rows[0]);
+rows[rows.length - 1] = 10;
+console.log(rows);
