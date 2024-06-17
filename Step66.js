a66c094b28ec70f89e46d7761d07d330213fd2e0
withdxng <breadnguyen2504@gmail.com>
2024-06-17 16:48:06 +0700

    Create Step66.js

---
+++ Step66.js
@@ -0,0 +1,19 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return character.repeat(rowNumber);
+}
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
