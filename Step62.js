6c787969662fc3c66d2289476118bbd2b8478c28
withdxng <breadnguyen2504@gmail.com>
2024-06-17 16:48:16 +0700

    Create Step62.js

---
+++ Step62.js
@@ -0,0 +1,20 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow() {
+
+}
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
