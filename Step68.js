a0a7436c0d12752c4bda11f121fa35caa4ca4edc
withdxng <breadnguyen2504@gmail.com>
2024-06-17 16:48:00 +0700

    Create Step68.js

---
+++ Step68.js
@@ -0,0 +1,20 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
