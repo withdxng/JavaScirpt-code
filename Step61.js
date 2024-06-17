a40164c1430d2a12142a7943b37e2ab02b2fa664
withdxng <breadnguyen2504@gmail.com>
2024-06-17 16:48:25 +0700

    Create Step61.js

---
+++ Step61.js
@@ -0,0 +1,21 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow() {
+
+}
+const call = padRow();
+console.log(call);
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
