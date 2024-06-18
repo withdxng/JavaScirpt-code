75015fe974adccd7c24b2ea029f4e67cc118e37d
withdxng <breadnguyen2504@gmail.com>
2024-06-18 10:48:27 +0700

    Create Step75.js

---
+++ Step75.js
@@ -0,0 +1,20 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
