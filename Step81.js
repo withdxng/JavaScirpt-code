77611d746ce5d8db5f376722474450cdaf400afe
withdxng <breadnguyen2504@gmail.com>
2024-06-18 10:49:09 +0700

    Create Step81.js

---
+++ Step81.js
@@ -0,0 +1,26 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if ("") {
+  console.log("Condition is true");
+} else if (5 < 10) {
+  console.log("5 is less than 10");
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
