8157efc2d600a1ef51504ee2b64e06dabc6e095c
withdxng <breadnguyen2504@gmail.com>
2024-06-18 10:48:23 +0700

    Create Step73.js

---
+++ Step73.js
@@ -0,0 +1,19 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i < count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
