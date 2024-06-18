568533601f8ba46205d7bc9a6af27860c81d37a1
withdxng <breadnguyen2504@gmail.com>
2024-06-18 10:48:18 +0700

    Create Step71.js

---
+++ Step71.js
@@ -0,0 +1,11 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 0; i < count; i++) {
+  rows.push(padRow(i + 1, count));
+}
